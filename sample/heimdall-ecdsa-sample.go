d8eb4f700d807cfdb033da32dfbdb7eaa91fbde0
dongin <needshield@gmail.com>
2018-04-13 10:48:26 +0900

    modify sample files for adding about ByteToKey function and add some comments for each functions.

--- sample/heimdall-ecdsa-sample.go
+++ sample/heimdall-ecdsa-sample.go
@@ -22,33 +22,31 @@ func main() {
 	keyManager, err := key.NewKeyManager("")
 	errorCheck(err)
 
-	keyPath := keyManager.GetPath()
-
 	defer os.RemoveAll("./.heimdall")
 
-	// if there is no key file in default key directory, then generate key.
-	if _, err := os.Stat(keyPath); os.IsNotExist(err) {
-		keyManager.GenerateKey(key.ECDSA384)
-		errorCheck(err)
-	}
-
-	pri, pub, err := keyManager.GetKey()
+	// Generate key pair with ECDSA algorithm.
+	pri, pub, err := keyManager.GenerateKey(key.ECDSA384)
 	errorCheck(err)
 
+	// Get key from memory of keyManager or from key files in key path of keyManager.
+	pri, pub, err = keyManager.GetKey()
+	errorCheck(err)
+
+	// Convert key to PEM(byte) format.
 	bytePriKey, err := pri.ToPEM()
 	bytePubKey, err := pub.ToPEM()
 
-	// reconstruct key pair in bytes to key.
+	// Reconstruct key pair from bytes to key.
 	err = keyManager.ByteToKey(bytePriKey, key.ECDSA384, key.PRIVATE_KEY)
 	err = keyManager.ByteToKey(bytePubKey, key.ECDSA384, key.PUBLIC_KEY)
 	errorCheck(err)
 
-	// get the reconstructed key pair.
+	// Get the reconstructed key pair.
 	recPri, recPub, err := keyManager.GetKey()
 
-	// compare reconstructed key pair with original key pair.
+	// Compare reconstructed key pair with original key pair.
 	if reflect.DeepEqual(pri, recPri) && reflect.DeepEqual(pub, recPub) {
-		print("recovoer complete!")
+		print("reconstruct complete!\n")
 	}
 
 	sampleData := []byte("This is sample data from heimdall.")
@@ -56,16 +54,21 @@ func main() {
 	hashManager, err := hashing.NewHashManager()
 	errorCheck(err)
 
+	// Convert raw data to digest(hash value) by using SHA512 function.
 	digest, err := hashManager.Hash(sampleData, nil, hashing.SHA512)
 	errorCheck(err)
 
 	authManager, err := auth.NewAuth()
 	errorCheck(err)
 
+	// AuthManager makes digest(hash value) to signature with private key.
 	signature, err := authManager.Sign(pri, digest, nil)
 	errorCheck(err)
 
 	/* --------- After data transmitted --------- */
+
+	// AuthManager verify that received data has any forgery during transmitting process by digest.
+	// and verify that the received data is surely from the expected sender by public key.
 	ok, err := authManager.Verify(pub, signature, digest, nil)
 	errorCheck(err)
 
