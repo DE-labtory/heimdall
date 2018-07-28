ec58a765271d93b1ce56ce4334c4a180ca77e2e7
dongin <needshield@gmail.com>
2018-07-28 16:16:59 +0900

    bring signing and verify functions from auth directory.

--- auth.go
+++ auth.go
@@ -1,20 +1,84 @@
 package heimdall
 
 import (
-	"crypto"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"math/big"
+	"encoding/asn1"
+	"errors"
 )
 
-// SignerOpts contains options for signing with a Signer.
-type SignerOpts interface {
-	crypto.SignerOpts
+
+// ecdsaSignature contains ECDSA signature components that are two integers, R and S.
+type ecdsaSignature struct {
+	R, S *big.Int
 }
 
-// signer represents subject of signing process.
-type Signer interface {
-	Sign(priKey Key, digest []byte, opts SignerOpts) ([]byte, error)
+// marshalECDSASignature returns encoding format (ASN.1) of signature.
+func marshalECDSASignature(r, s *big.Int) ([]byte, error) {
+	return asn1.Marshal(ecdsaSignature{r, s})
 }
 
-// verifier represents subject of verifying process.
-type Verifier interface {
-	Verify(pubKey Key, signature, digest []byte, opts SignerOpts) (bool, error)
+// unmarshalECDSASignature parses the ASN.1 structure to ECDSA signature.
+func unmarshalECDSASignature(signature []byte) (*big.Int, *big.Int, error) {
+	ecdsaSig := new(ecdsaSignature)
+	rest, err := asn1.Unmarshal(signature, ecdsaSig)
+	if err != nil {
+		return nil, nil, errors.New("failed to unmarshal")
+	}
+
+	if len(rest) != 0 {
+		return nil, nil, errors.New("garbage following signature")
+	}
+
+	if ecdsaSig.R == nil {
+		return nil, nil, errors.New("invalid signature")
+	}
+
+	if ecdsaSig.S == nil {
+		return nil, nil, errors.New("invalid signature")
+	}
+
+	if ecdsaSig.R.Sign() != 1 {
+		return nil, nil, errors.New("invalid signature")
+	}
+
+	if ecdsaSig.S.Sign() != 1 {
+		return nil, nil, errors.New("invalid signature")
+	}
+
+	return ecdsaSig.R, ecdsaSig.S, nil
+}
+
+// Sign signs a digest(hash) using priKey(private key), and returns signature.
+func Sign(pri *ecdsa.PrivateKey, digest []byte) ([]byte, error) {
+	r, s, err := ecdsa.Sign(rand.Reader, pri, digest)
+	if err != nil {
+		return nil, err
+	}
+
+	signature, err := marshalECDSASignature(r, s)
+	if err != nil {
+		return nil, err
+	}
+
+	// remove private key from memory.
+	defer RemoveKeyMem(pri)
+
+	return signature, nil
+}
+
+// Verify verifies the signature using pubKey(public key) and digest of original message, then returns boolean value.
+func Verify(pub *ecdsa.PublicKey, signature, digest []byte) (bool, error) {
+	r, s, err := unmarshalECDSASignature(signature)
+	if err != nil {
+		return false, err
+	}
+
+	valid := ecdsa.Verify(pub, digest, r, s)
+	if !valid {
+		return valid, errors.New("invalid signature")
+	}
+
+	return valid, nil
 }
