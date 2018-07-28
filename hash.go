a1b6a9ee1ea63486b08724475c1c74a2d2171b8f
dongin <needshield@gmail.com>
2018-07-28 16:15:56 +0900

    move dir to heimdall and modify hash variable to solve warning.

---
+++ hash.go
@@ -0,0 +1,39 @@
+// This file implements hash manager for hashing process.
+
+package heimdall
+
+import (
+	"crypto/sha1"
+	"crypto/sha512"
+	"errors"
+	"hash"
+)
+
+// Hash hashes the input data.
+func Hash(data []byte, tail []byte, opts HashOpts) ([]byte, error) {
+
+	if data == nil {
+		return nil, errors.New("data should not be NIL")
+	}
+
+	var selectedHash hash.Hash
+
+	switch opts {
+	case SHA1:
+		selectedHash = sha1.New()
+	case SHA224:
+		selectedHash = sha512.New512_224()
+	case SHA256:
+		selectedHash = sha512.New512_256()
+	case SHA384:
+		selectedHash = sha512.New384()
+	case SHA512:
+		selectedHash = sha512.New()
+	default:
+		return nil, errors.New("invalid hash opts")
+	}
+
+	selectedHash.Write(data)
+	return selectedHash.Sum(tail), nil
+
+}
