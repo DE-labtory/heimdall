cf546e593e451082e3266c763bee9eb61d1d2264
dongin <needshield@gmail.com>
2018-07-30 13:20:42 +0900

    add and modify errorcheck codes for checking errors more accurately.

--- sample/ecdsa/heimdall-ecdsa-sample.go
+++ sample/ecdsa/heimdall-ecdsa-sample.go
@@ -6,7 +6,7 @@ import (
 	"github.com/it-chain/heimdall"
 	"os"
 	"encoding/hex"
-	"bytes"
+	"errors"
 )
 
 /*
@@ -31,7 +31,9 @@ func main() {
 	fmt.Println("genereted private key bytes : ", hex.EncodeToString(bytePri))
 
 	if recPri.D.Cmp(pri.D) == 0 && recPri.X.Cmp(pri.X) == 0 && recPri.Y.Cmp(pri.Y) == 0 {
-		fmt.Println("converting private key to byte(and byte to private key) format success")
+		fmt.Println("obtaining private key from byte format of private key's D component is success")
+	} else {
+		errorCheck(errors.New("obtaining private key from byte format of private key's D component is failed"))
 	}
 
 	// public key to bytes(from bytes)
@@ -39,14 +41,16 @@ func main() {
 	bytePub := heimdall.PubKeyToBytes(pub)
 	recPub, err := heimdall.BytesToPubKey(bytePub, curveOpt)
 
-	if recPub.X.Cmp(pub.X) == 0 && recPub.Y.Cmp(pub.Y) == 0 {
-		fmt.Println("converting public key to byte(and byte to public key) format success")
+	if recPub.X.Cmp(pub.X) == 0 && recPub.Y.Cmp(pub.Y) == 0 && recPub.Curve.IsOnCurve(pub.X, pub.Y) {
+		fmt.Println("obtaining public key from public key's X, Y coordinate is success")
+	} else {
+		errorCheck(errors.New("obtaining public key from public key's X, Y coordinate is failed"))
 	}
 
 	// make new keystore
 	ks, err := heimdall.NewKeyStore(heimdall.TestKeyDir)
 	errorCheck(err)
-	fmt.Println("make new keystore success")
+	fmt.Println("making new keystore is success")
 
 	// storing key
 	err = ks.StoreKey(pri, "password")
@@ -55,18 +59,22 @@ func main() {
 
 	// public key ---> SKI ---->  Key ID (Base58encoded SKI)
 	ski := heimdall.SKIFromPubKey(pub)
-	keyId := heimdall.PubKeyToKeyID(pub)
+	keyId := heimdall.SKIToKeyID(ski)
+	errorCheck(heimdall.KeyIDPrefixCheck(keyId))
 	fmt.Println("keyID : ", len(keyId), keyId)
 	// key ID ---> SKI
 	recSki := heimdall.SKIFromKeyID(keyId)
-	if bytes.Compare(ski, recSki) == 0 {
-		fmt.Println("key id to(from) ski success")
-	}
+	errorCheck(heimdall.SKIValidCheck(keyId, hex.EncodeToString(recSki)))
+
+	fmt.Println("key id to(from) ski success")
 
 	// load private key by key id and password
 	loadedPri, err := ks.LoadKey(keyId, "password")
+	errorCheck(err)
 	if loadedPri.D.Cmp(pri.D) == 0 && loadedPri.X.Cmp(pri.X) == 0 && loadedPri.Y.Cmp(pri.Y) == 0 {
-		fmt.Println("loading private key from key id success")
+		fmt.Println("loading private key by key id success")
+	} else {
+		errorCheck(errors.New("loading private key by key id failed"))
 	}
 
 	fmt.Println("loaded private key bytes : ", hex.EncodeToString(heimdall.PriKeyToBytes(loadedPri)))
