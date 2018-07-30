2ac5747f1ca1641847e101300abff85123e0c81f
dongin <needshield@gmail.com>
2018-07-30 12:39:19 +0900

    add key id prefix as a constant.

--- definition.go
+++ definition.go
@@ -6,11 +6,17 @@ import (
 	"encoding/hex"
 )
 
+
+// Key ID prefix
+const keyIDPrefix = "IT"
+
+// directories for test
 var WorkingDir, _ = os.Getwd()
 var RootDir = filepath.Dir(WorkingDir)
 var TestKeyDir = filepath.Join(WorkingDir, "./.testKeys")
 
-var TestCurveOpt = SECP256R1
+// Parameters for test
+const TestCurveOpt = SECP256R1
 
 // Note: salt have to be unique, so do not use this for real implementation.
 var TestSalt = []byte{0xc8, 0x28, 0xf2, 0x58, 0xa7, 0x6a, 0xad, 0x7b}
