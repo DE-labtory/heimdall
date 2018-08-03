cc529e3dae157fa9ce6482882b74ef5ea3236057
dongin <needshield@gmail.com>
2018-08-03 16:19:11 +0900

    add test functions for newly added functions for verifying cert chain and formatting certificate.

--- certificate_test.go
+++ certificate_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"crypto/x509"
 	"crypto/rand"
-	"encoding/pem"
+	"os"
 )
 
 
@@ -35,36 +35,112 @@ func TestNewCertStore(t *testing.T) {
 func TestCertStore_StoreCert(t *testing.T) {
 	certStore, _ := NewCertStore(TestCertDir)
 	pri, _ := GenerateKey(TestCurveOpt)
-	derBytes, err := x509.CreateCertificate(rand.Reader, &testCertTemplate, &testCertTemplate, &pri.PublicKey, pri)
+	derBytes, err := x509.CreateCertificate(rand.Reader, &TestRootCertTemplate, &TestRootCertTemplate, &pri.PublicKey, pri)
 	assert.NoError(t, err)
 
-	pemBytes := pem.EncodeToMemory(&pem.Block{
-		Type: "CERTIFICATE",
-		Bytes: derBytes,
-	})
+	cert, _ := DERToX509Cert(derBytes)
 
-	err = certStore.StoreCert(pemBytes)
+	err = certStore.StoreCert(cert)
 	assert.NoError(t, err)
-	// defer os.RemoveAll(TestCert)
+
+	defer os.RemoveAll(TestCertDir)
 }
 
 func TestCertStore_LoadCert(t *testing.T) {
 	certStore, _ := NewCertStore(TestCertDir)
 	pri, _ := GenerateKey(TestCurveOpt)
-	testCertTemplate.SubjectKeyId = SKIFromPubKey(&pri.PublicKey)
-	derBytes, err := x509.CreateCertificate(rand.Reader, &testCertTemplate, &testCertTemplate, &pri.PublicKey, pri)
+	TestRootCertTemplate.SubjectKeyId = SKIFromPubKey(&pri.PublicKey)
+	derBytes, err := x509.CreateCertificate(rand.Reader, &TestRootCertTemplate, &TestRootCertTemplate, &pri.PublicKey, pri)
 	assert.NoError(t, err)
 
-	pemBytes := pem.EncodeToMemory(&pem.Block{
-		Type: "CERTIFICATE",
-		Bytes: derBytes,
-	})
+	cert, _ := DERToX509Cert(derBytes)
 
-	err = certStore.StoreCert(pemBytes)
+	err = certStore.StoreCert(cert)
 
 	certStore, _ = NewCertStore(TestCertDir)
 	testCert, err := certStore.LoadCert(PubKeyToKeyID(&pri.PublicKey))
 	assert.NoError(t, err)
 	assert.NotNil(t, testCert)
-	// defer os.RemoveAll(TestCert)
+
+	defer os.RemoveAll(TestCertDir)
+}
+
+func TestPemToX509Cert(t *testing.T) {
+	pri, _ := GenerateKey(TestCurveOpt)
+	TestRootCertTemplate.SubjectKeyId = SKIFromPubKey(&pri.PublicKey)
+	derBytes, err := x509.CreateCertificate(rand.Reader, &TestRootCertTemplate, &TestRootCertTemplate, &pri.PublicKey, pri)
+	assert.NoError(t, err)
+	pemBytes := DERCertToPem(derBytes)
+
+	cert, err := PemToX509Cert(pemBytes)
+	assert.NoError(t, err)
+	assert.NotNil(t, cert)
+}
+
+func TestX509CertToPem(t *testing.T) {
+	cert, _ := PemToX509Cert([]byte(TestCertPemBytes))
+	pemBytes := X509CertToPem(cert)
+	assert.Equal(t, pemBytes, []byte(TestCertPemBytes))
+}
+
+func TestDERCertToPem(t *testing.T) {
+	cert, _ := PemToX509Cert([]byte(TestCertPemBytes))
+	pemBytes := DERCertToPem(cert.Raw)
+	assert.Equal(t, pemBytes, []byte(TestCertPemBytes))
+}
+
+func TestX509CertToDER(t *testing.T) {
+	cert, _ := PemToX509Cert([]byte(TestCertPemBytes))
+	derBytes := X509CertToDER(cert)
+	assert.NotNil(t, derBytes)
+	assert.Equal(t, []byte(TestCertPemBytes), DERCertToPem(derBytes))
+}
+
+func TestDERToX509Cert(t *testing.T) {
+	cert, _ := PemToX509Cert([]byte(TestCertPemBytes))
+	assert.NotNil(t, cert)
+	assert.Equal(t, []byte(TestCertPemBytes), X509CertToPem(cert))
+}
+
+func TestCertStore_VerifyCertChain(t *testing.T) {
+	certStore, _ := NewCertStore(TestCertDir)
+
+	// root cert
+	rootPri, _ := GenerateKey(TestCurveOpt)
+
+	TestRootCertTemplate.SubjectKeyId = SKIFromPubKey(&rootPri.PublicKey)
+	derBytes, err := x509.CreateCertificate(rand.Reader, &TestRootCertTemplate, &TestRootCertTemplate, &rootPri.PublicKey, rootPri)
+	assert.NoError(t, err)
+	rootCert, _ := DERToX509Cert(derBytes)
+
+	err = certStore.StoreCert(rootCert)
+	assert.NoError(t, err)
+
+	// intermediate cert
+	interPri, _ := GenerateKey(TestCurveOpt)
+
+	TestIntermediateCertTemplate.SubjectKeyId = SKIFromPubKey(&interPri.PublicKey)
+	derBytes, err = x509.CreateCertificate(rand.Reader, &TestIntermediateCertTemplate, &TestRootCertTemplate, &interPri.PublicKey, rootPri)
+	assert.NoError(t, err)
+	interCert, _ := DERToX509Cert(derBytes)
+
+	err = certStore.StoreCert(interCert)
+	assert.NoError(t, err)
+
+	// client cert
+	ClientPri, _ := GenerateKey(TestCurveOpt)
+
+	TestCertTemplate.SubjectKeyId = SKIFromPubKey(&ClientPri.PublicKey)
+	derBytes, err = x509.CreateCertificate(rand.Reader, &TestCertTemplate, &TestIntermediateCertTemplate, &ClientPri.PublicKey, interPri)
+	assert.NoError(t, err)
+	clientCert, _ := DERToX509Cert(derBytes)
+
+	err = certStore.StoreCert(clientCert)
+	assert.NoError(t, err)
+
+	// verify certificate chain
+	err = certStore.VerifyCertChain(clientCert)
+	assert.NoError(t, err)
+
+	defer os.RemoveAll(TestCertDir)
 }
