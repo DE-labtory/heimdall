c3758fe08c4e4c34626dc9220611690fa0f38c2f
dongin <needshield@gmail.com>
2018-08-13 14:03:53 +0900

    remove test codes for other KDFs.

--- keyDerivation_test.go
+++ keyDerivation_test.go
@@ -20,21 +20,10 @@ package heimdall
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
-	"strconv"
 )
 
 func TestDeriveKeyFromPwd(t *testing.T) {
-	dKey, err := DeriveKeyFromPwd("scrypt", []byte("password"), TestScrpytParams)
+	dKey, err := DeriveKeyFromPwd(TestConf.kdf, []byte("password"), TestSalt, TestConf.encKeyLength, TestConf.kdfParams)
 	assert.NoError(t, err)
 	assert.NotNil(t, dKey)
-	keyLen, _ := strconv.Atoi(ScryptKeyLen)
-	assert.Len(t, dKey, keyLen)
-
-	dKey, err = DeriveKeyFromPwd("pbkdf2", []byte("password"), TestScrpytParams)
-	assert.Error(t, err)
-	assert.Nil(t, dKey)
-
-	dKey, err = DeriveKeyFromPwd("mykdf", []byte("password"), TestScrpytParams)
-	assert.Error(t, err)
-	assert.Nil(t, dKey)
 }
