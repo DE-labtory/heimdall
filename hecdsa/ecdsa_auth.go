75fd5d9dce717f0f8fd4adf4504004613057b880
dongin <needshield@gmail.com>
2018-09-27 17:32:19 +0900

    implement ecdsa authentication functions (signer, verifier, certverifier)

---
+++ hecdsa/ecdsa_auth.go
@@ -0,0 +1,283 @@
+/*
+ * Copyright 2018 It-chain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// This file provides ECDSA signing and verifying related functions.
+
+package hecdsa
+
+import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"encoding/asn1"
+	"errors"
+	"math/big"
+
+	"bytes"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"time"
+
+	"github.com/it-chain/heimdall"
+	"github.com/it-chain/heimdall/hashing"
+)
+
+// todo: 에러 메시지 정리
+
+// ecdsaSignature contains ECDSA signature components that are two big integers, R and S.
+type ecdsaSignature struct {
+	R, S *big.Int
+}
+
+// marshalECDSASignature returns encoding format (ASN.1) of signature.
+func marshalECDSASignature(r, s *big.Int) ([]byte, error) {
+	return asn1.Marshal(ecdsaSignature{r, s})
+}
+
+// unmarshalECDSASignature parses the ASN.1 structure to ECDSA signature.
+func unmarshalECDSASignature(signature []byte) (*big.Int, *big.Int, error) {
+	ecdsaSig := new(ecdsaSignature)
+	rest, err := asn1.Unmarshal(signature, ecdsaSig)
+	if err != nil {
+		return nil, nil, errors.New("failed to unmarshal")
+	}
+
+	if len(rest) != 0 {
+		return nil, nil, errors.New("garbage following signature")
+	}
+
+	if ecdsaSig.R == nil {
+		return nil, nil, errors.New("invalid signature")
+	}
+
+	if ecdsaSig.S == nil {
+		return nil, nil, errors.New("invalid signature")
+	}
+
+	if ecdsaSig.R.Sign() != 1 {
+		return nil, nil, errors.New("invalid signature")
+	}
+
+	if ecdsaSig.S.Sign() != 1 {
+		return nil, nil, errors.New("invalid signature")
+	}
+
+	return ecdsaSig.R, ecdsaSig.S, nil
+}
+
+type Signer struct {
+}
+
+// Sign generates signature for a data using private key.
+// if preBuf is not nil, data's hashing append to preBuf - no malloc for hashed data.
+func (signer *Signer) Sign(pri heimdall.PriKey, message []byte, opts heimdall.SignerOpts) ([]byte, error) {
+	digest, err := hashing.Hash(message, opts.HashOpt())
+	if err != nil {
+		return nil, err
+	}
+
+	r, s, err := ecdsa.Sign(rand.Reader, pri.(*PriKey).internalPriKey, digest)
+	if err != nil {
+		return nil, err
+	}
+
+	signature, err := marshalECDSASignature(r, s)
+	if err != nil {
+		return nil, err
+	}
+
+	// remove private key from memory.
+	defer pri.Clear()
+
+	return signature, nil
+}
+
+type Verifier struct {
+}
+
+// Verify verifies the signature using pubKey(public key) and digest of original message, then returns boolean value.
+func (verifier *Verifier) Verify(pub heimdall.PubKey, signature, message []byte, opts heimdall.SignerOpts) (bool, error) {
+	digest, err := hashing.Hash(message, opts.HashOpt())
+	if err != nil {
+		return false, err
+	}
+
+	r, s, err := unmarshalECDSASignature(signature)
+	if err != nil {
+		return false, err
+	}
+
+	valid := ecdsa.Verify(pub.(*PubKey).internalPubKey, digest, r, s)
+	return valid, nil
+}
+
+// VerifyWithCert verify a signature with certificate.
+func (verifier *Verifier) VerifyWithCert(cert *x509.Certificate, signature, message []byte, opts heimdall.SignerOpts) (bool, error) {
+	pub := NewPubKey(cert.PublicKey.(*ecdsa.PublicKey))
+	return verifier.Verify(pub, signature, message, opts)
+}
+
+type CertVerifier struct {
+}
+
+// VerifyCertChain verifies a certificate from local certificates in certificate store directory.
+func (cv *CertVerifier) VerifyCertChain(cert *x509.Certificate, certDirPath string) error {
+	roots, err := makeRootsPool(certDirPath)
+	if err != nil {
+		return err
+	}
+
+	intermediates, err := makeIntermediatesPool(certDirPath)
+	if err != nil {
+		return err
+	}
+
+	opts := x509.VerifyOptions{
+		Roots:         roots,
+		Intermediates: intermediates,
+	}
+
+	_, err = cert.Verify(opts)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// makeRootsPool makes certificate pool of root certificates in certificate store directory.
+func makeRootsPool(certDirPath string) (rootsPool *x509.CertPool, err error) {
+	rootsPool = x509.NewCertPool()
+
+	files, err := ioutil.ReadDir(certDirPath)
+	if err != nil {
+		return nil, errors.New("invalid cert directory path - failed to read directory path")
+	}
+
+	for _, file := range files {
+		certFilePath := filepath.Join(certDirPath, file.Name())
+		certPEMBlock, err := ioutil.ReadFile(certFilePath)
+		if err != nil {
+			return nil, err
+		}
+
+		cert, err := heimdall.PemToX509Cert(certPEMBlock)
+		if cert.IsCA == true && bytes.Compare(cert.RawIssuer, cert.RawSubject) == 0 {
+			rootsPool.AddCert(cert)
+		}
+	}
+
+	if len(rootsPool.Subjects()) == 0 {
+		return nil, errors.New("no root certificate in certificate store directory")
+	}
+
+	return rootsPool, nil
+}
+
+// makeIntermediatesPool makes certificate pool of intermediate certificates in certificate store directory.
+func makeIntermediatesPool(certDirPath string) (intermediatesPool *x509.CertPool, err error) {
+	intermediatesPool = x509.NewCertPool()
+
+	files, err := ioutil.ReadDir(certDirPath)
+	if err != nil {
+		return nil, errors.New("invalid cert directory path - failed to read directory path")
+	}
+
+	for _, file := range files {
+		certFilePath := filepath.Join(certDirPath, file.Name())
+		certPEMBlock, err := ioutil.ReadFile(certFilePath)
+		if err != nil {
+			return nil, err
+		}
+
+		cert, err := heimdall.PemToX509Cert(certPEMBlock)
+		if cert.IsCA == true && bytes.Compare(cert.RawIssuer, cert.RawSubject) != 0 {
+			intermediatesPool.AddCert(cert)
+		}
+	}
+
+	return intermediatesPool, nil
+}
+
+// VerifyCert verifies a certificate's validity.
+func (cv *CertVerifier) VerifyCert(cert *x509.Certificate) error {
+	// check if expired or invalid generation time
+	err := checkTime(cert.NotBefore, cert.NotAfter)
+	if err != nil {
+		return err
+	}
+
+	// check if revoked
+	for _, url := range cert.CRLDistributionPoints {
+		crl, err := requestCRL(url)
+		if err != nil {
+			return err
+		}
+
+		err = checkRevocation(cert, crl)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// checkTime checks if entered certificate's generated/expired time is valid.
+func checkTime(notBefore time.Time, notAfter time.Time) error {
+	if time.Now().Before(notBefore) {
+		return errors.New("invalid certificate - certificate's generated time is invalid")
+	}
+
+	if time.Now().After(notAfter) {
+		return errors.New("invalid certificate - certificate is expired")
+	}
+
+	return nil
+}
+
+// requestCRL requests CRL(Certificate Revocation List) from CRLDistributionURL.
+func requestCRL(url string) (*pkix.CertificateList, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	} else if resp.StatusCode >= 300 {
+		return nil, errors.New("failed to retrieve CRL")
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return x509.ParseCRL(body)
+}
+
+// checkRevocation checks if entered certificate is revoked by CRL(Certificate Revocation List).
+func checkRevocation(cert *x509.Certificate, crl *pkix.CertificateList) error {
+	for _, revokedCert := range crl.TBSCertList.RevokedCertificates {
+		if cert.SerialNumber.Cmp(revokedCert.SerialNumber) == 0 {
+			return errors.New("invalid certificate - revoked certificate")
+		}
+	}
+
+	return nil
+}
---
+++ hecdsa/ecdsa_auth_test.go
@@ -0,0 +1,238 @@
+/*
+ * Copyright 2018 It-chain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hecdsa_test
+
+import (
+	"testing"
+
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+
+	"os"
+
+	"crypto/x509/pkix"
+	"io"
+	"math/big"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
+	"github.com/it-chain/heimdall"
+	"github.com/it-chain/heimdall/hecdsa"
+	"github.com/it-chain/heimdall/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func setUpSigner(t *testing.T) (hecdsa.Signer, *hecdsa.PriKey, hecdsa.SignerOpts) {
+	ecdsaSigner := hecdsa.Signer{}
+	generator := hecdsa.KeyGenerator{}
+
+	pri, err := generator.GenerateKey(hecdsa.ECP384)
+	assert.NoError(t, err)
+
+	signerOpt := hecdsa.SignerOpts("SHA384")
+	assert.True(t, signerOpt.IsValid())
+
+	return ecdsaSigner, pri.(*hecdsa.PriKey), signerOpt
+}
+
+func TestSigner_Sign(t *testing.T) {
+	// given
+	ecdsaSigner, pri, signerOpt := setUpSigner(t)
+	message := []byte("hello")
+
+	// when
+	signature, noErr := ecdsaSigner.Sign(pri, message, signerOpt)
+	nilSig, err := ecdsaSigner.Sign(pri, nil, signerOpt)
+
+	// then
+	assert.NotNil(t, signature)
+	assert.NoError(t, noErr)
+	assert.Nil(t, nilSig)
+	assert.Error(t, err)
+}
+
+func TestVerifier_Verify(t *testing.T) {
+	// given
+	ecdsaSigner, pri, signerOpt := setUpSigner(t)
+	otherPri := setUpPriKey(t)
+	otherPub := otherPri.PublicKey()
+
+	message := []byte("hello")
+	wrongMessage := []byte("this is wrong meesage")
+
+	signature, err := ecdsaSigner.Sign(pri, message, signerOpt)
+	assert.NotNil(t, signature)
+	assert.NoError(t, err)
+
+	pub := pri.PublicKey()
+
+	ecdsaVerifier := hecdsa.Verifier{}
+
+	// when
+	valid, noErr := ecdsaVerifier.Verify(pub, signature, message, signerOpt)
+	inValid, _ := ecdsaVerifier.Verify(pub, signature, wrongMessage, signerOpt)
+	inValid2, _ := ecdsaVerifier.Verify(otherPub, signature, message, signerOpt)
+	_, err = ecdsaVerifier.Verify(pub, []byte("wrong sig"), message, signerOpt)
+	_, err2 := ecdsaVerifier.Verify(pub, signature, nil, signerOpt)
+
+	// then
+	assert.NoError(t, noErr)
+	assert.Error(t, err)
+	assert.Error(t, err2)
+
+	assert.True(t, valid)
+	assert.False(t, inValid)
+	assert.False(t, inValid2)
+}
+
+func TestVerifier_VerifyWithCert(t *testing.T) {
+	// given
+	ecdsaSigner, _, signerOpt := setUpSigner(t)
+
+	message := []byte("hello")
+
+	ecdsaVerifier := hecdsa.Verifier{}
+
+	//root cert
+	rootPri, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	rootPub := &rootPri.PublicKey
+	assert.NoError(t, err)
+	hPri := hecdsa.NewPriKey(rootPri)
+
+	signature, err := ecdsaSigner.Sign(hPri, message, signerOpt)
+	assert.NotNil(t, signature)
+	assert.NoError(t, err)
+
+	mocks.TestRootCertTemplate.SubjectKeyId = hPri.SKI()
+	derBytes, err := x509.CreateCertificate(rand.Reader, &mocks.TestRootCertTemplate, &mocks.TestRootCertTemplate, rootPub, rootPri)
+	assert.NoError(t, err)
+	rootCert, _ := heimdall.DERToX509Cert(derBytes)
+
+	// when
+	valid, NoErr := ecdsaVerifier.VerifyWithCert(rootCert, signature, message, signerOpt)
+
+	// then
+	assert.NoError(t, NoErr)
+	assert.True(t, valid)
+}
+
+func TestCertVerifier_VerifyCertChain(t *testing.T) {
+	// given
+	certVerifier := hecdsa.CertVerifier{}
+	certStorer := hecdsa.CertStorer{}
+
+	//root cert
+	rootPri, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	rootPub := &rootPri.PublicKey
+	assert.NoError(t, err)
+	hRootPri := hecdsa.NewPriKey(rootPri)
+
+	mocks.TestRootCertTemplate.SubjectKeyId = hRootPri.SKI()
+	derBytes, err := x509.CreateCertificate(rand.Reader, &mocks.TestRootCertTemplate, &mocks.TestRootCertTemplate, rootPub, rootPri)
+	assert.NoError(t, err)
+	rootCert, _ := heimdall.DERToX509Cert(derBytes)
+
+	err = certStorer.StoreCert(rootCert, heimdall.TestCertDir)
+	assert.NoError(t, err)
+
+	// intermediate cert
+	interPri, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	interPub := &interPri.PublicKey
+	assert.NoError(t, err)
+	hInterPri := hecdsa.NewPriKey(interPri)
+
+	mocks.TestIntermediateCertTemplate.SubjectKeyId = hInterPri.SKI()
+	derBytes, err = x509.CreateCertificate(rand.Reader, &mocks.TestIntermediateCertTemplate, &mocks.TestRootCertTemplate, interPub, rootPri)
+	assert.NoError(t, err)
+	interCert, _ := heimdall.DERToX509Cert(derBytes)
+
+	err = certStorer.StoreCert(interCert, heimdall.TestCertDir)
+	assert.NoError(t, err)
+
+	// client cert
+	clientPri, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	clientPub := &clientPri.PublicKey
+	assert.NoError(t, err)
+	hClientPri := hecdsa.NewPriKey(clientPri)
+
+	mocks.TestIntermediateCertTemplate.SubjectKeyId = hClientPri.SKI()
+	derBytes, err = x509.CreateCertificate(rand.Reader, &mocks.TestCertTemplate, &mocks.TestIntermediateCertTemplate, clientPub, interPri)
+	assert.NoError(t, err)
+	clientCert, _ := heimdall.DERToX509Cert(derBytes)
+
+	// when
+	err = certVerifier.VerifyCertChain(clientCert, heimdall.TestCertDir)
+	assert.NoError(t, err)
+
+	defer os.RemoveAll(heimdall.TestCertDir)
+}
+
+func TestCertVerifier_VerifyCert(t *testing.T) {
+	// given
+	certVerifier := hecdsa.CertVerifier{}
+
+	// expired cert
+	expiredCert, _ := heimdall.PemToX509Cert([]byte(mocks.ExpiredCertForTest))
+	// revoked cert
+	revokedCert, _ := heimdall.PemToX509Cert([]byte(mocks.RevokedCertForTest))
+	// normal client cert
+	clientCert, _ := heimdall.PemToX509Cert([]byte(mocks.ClientCertForTest))
+
+	// root cert
+	rootPri, _ := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	rootPub := &rootPri.PublicKey
+	hRootPub := hecdsa.NewPubKey(rootPub)
+
+	mocks.TestRootCertTemplate.SubjectKeyId = hRootPub.SKI()
+	derBytes, err := x509.CreateCertificate(rand.Reader, &mocks.TestRootCertTemplate, &mocks.TestRootCertTemplate, rootPub, rootPri)
+	assert.NoError(t, err)
+	rootCert, _ := heimdall.DERToX509Cert(derBytes)
+
+	// revoked certificate setting
+	revokedCertificate := new(pkix.RevokedCertificate)
+	revokedCertificate.SerialNumber = big.NewInt(44)
+	revokedCertificate.RevocationTime = time.Now()
+	revokedCertificate.Extensions = nil
+
+	revokedCertList := []pkix.RevokedCertificate{*revokedCertificate}
+
+	// create CRL (Certificate Revocation List)
+	crlBytes, err := rootCert.CreateCRL(rand.Reader, rootPri, revokedCertList, time.Now(), time.Now().Add(time.Hour*24))
+	assert.NoError(t, err)
+	assert.NotNil(t, crlBytes)
+
+	// httptest server for testing
+	testCA := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, string(crlBytes))
+	}))
+
+	revokedCert.CRLDistributionPoints = []string{testCA.URL}
+	clientCert.CRLDistributionPoints = []string{testCA.URL}
+
+	// when
+	expiredErr := certVerifier.VerifyCert(expiredCert)
+	revokedErr := certVerifier.VerifyCert(revokedCert)
+	clientErr := certVerifier.VerifyCert(clientCert)
+
+	// then
+	assert.Error(t, expiredErr)
+	assert.Error(t, revokedErr)
+	assert.NoError(t, clientErr)
+}
