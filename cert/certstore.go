8deb73a3d2615a115e7ad10efdaedb816784b4ad
dongin <needshield@gmail.com>
2018-10-03 15:27:46 +0900

    change package name from certstore to cert

--- cert/certstore.go
+++ cert/certstore.go
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package certstore
+package cert
 
 import (
 	"crypto/ecdsa"
--- cert/certstore_test.go
+++ cert/certstore_test.go
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package certstore_test
+package cert_test
 
 import (
 	"crypto/ecdsa"
