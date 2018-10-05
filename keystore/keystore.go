d40a4a072d4c528536317d70946209d220e2e821
dongin <needshield@gmail.com>
2018-10-05 20:40:57 +0900

    modify for removed interfaces(keystore related, option related) and changed key implementation

--- keystore/keystore.go
+++ keystore/keystore.go
@@ -30,11 +30,11 @@ import (
 	"strings"
 
 	"github.com/it-chain/heimdall"
+	"github.com/it-chain/heimdall/encryption"
 	"github.com/it-chain/heimdall/kdf"
 )
 
 var ErrInvalidKeyGenOpt = errors.New("invalid ECDSA key generation option - not supported curve")
-var ErrInvalidKDFOpt = errors.New("invalid key derivation option")
 var ErrWrongKeyID = errors.New("wrong key id - failed to find key using key ID")
 var ErrEmptyKeyPath = errors.New("invalid keyPath - keyPath empty")
 
@@ -42,36 +42,20 @@ var ErrEmptyKeyPath = errors.New("invalid keyPath - keyPath empty")
 type KeyFile struct {
 	SKI          []byte
 	KeyGenOpt    string
-	KeyType      int
+	IsPrivate    bool
 	EncryptedKey string
-	Hints        EncryptionHints
+	Hints        *EncryptionHints
 }
 
 // struct for providing hints of encryption and key derivation function.
 type EncryptionHints struct {
-	EncInnerFileInfo heimdall.EncInnerFileInfo
-	KDFInnerFileInfo heimdall.KDFInnerFileInfo
-	KDFSalt          []byte
-}
-
-type KeyStorer struct {
-	kdfOpt heimdall.KeyDerivationOpts
-	encOpt heimdall.EncryptOpts
-	heimdall.KeyDeriver
-	heimdall.KeyEncryptor
-}
-
-func NewKeyStorer(kdfOpt heimdall.KeyDerivationOpts, encOpt heimdall.EncryptOpts, keyDeriver heimdall.KeyDeriver, keyEncryptor heimdall.KeyEncryptor) heimdall.KeyStorer {
-	return &KeyStorer{
-		kdfOpt:       kdfOpt,
-		encOpt:       encOpt,
-		KeyDeriver:   keyDeriver,
-		KeyEncryptor: keyEncryptor,
-	}
+	EncOpt  *encryption.Opts
+	KDFOpt  *kdf.Opts
+	KDFSalt []byte
 }
 
 // StoreKey stores private key that is encrypted by key derived from input password.
-func (keyStorer *KeyStorer) StoreKey(key heimdall.Key, pwd string, keyDirPath string) error {
+func StoreKey(key heimdall.Key, pwd string, keyDirPath string, encOpt *encryption.Opts, kdfOpt *kdf.Opts) error {
 	ski := key.SKI()
 	keyId := key.ID()
 
@@ -80,7 +64,7 @@ func (keyStorer *KeyStorer) StoreKey(key heimdall.Key, pwd string, keyDirPath st
 		return ErrInvalidKeyGenOpt
 	}
 
-	keyFilePath, err := keyStorer.makeKeyFilePath(keyId, keyDirPath)
+	keyFilePath, err := makeKeyFilePath(keyId, keyDirPath)
 	if err != nil {
 		return err
 	}
@@ -91,17 +75,19 @@ func (keyStorer *KeyStorer) StoreKey(key heimdall.Key, pwd string, keyDirPath st
 		return err
 	}
 
-	dKey, err := keyStorer.DeriveKey([]byte(pwd), salt, keyStorer.encOpt.KeyLen(), keyStorer.kdfOpt)
+	dKey, err := kdf.DeriveKey([]byte(pwd), salt, encOpt.KeyLen, kdfOpt)
 	if err != nil {
 		return err
 	}
 
-	encryptedKeyBytes, err := keyStorer.EncryptKey(key, dKey)
+	encryptedKeyBytes, err := encryption.EncryptKey(key, dKey, encOpt)
 	if err != nil {
 		return err
 	}
 
-	jsonKeyFile, err := keyStorer.makeJsonKeyFile(salt, ski, keyGenOpt, encryptedKeyBytes, key.KeyType())
+	encHints := makeEncryptionHints(encOpt, kdfOpt, salt)
+
+	jsonKeyFile, err := makeJsonKeyFile(encHints, ski, keyGenOpt, encryptedKeyBytes, key.IsPrivate())
 	if err != nil {
 		return err
 	}
@@ -117,7 +103,7 @@ func (keyStorer *KeyStorer) StoreKey(key heimdall.Key, pwd string, keyDirPath st
 }
 
 // makeKeyFilePath makes key file path (absolute) of the key file.
-func (keyStorer *KeyStorer) makeKeyFilePath(keyFileName string, keyDirPath string) (string, error) {
+func makeKeyFilePath(keyFileName string, keyDirPath string) (string, error) {
 	if _, err := os.Stat(keyDirPath); os.IsNotExist(err) {
 		err = os.MkdirAll(keyDirPath, 0755)
 		if err != nil {
@@ -128,18 +114,20 @@ func (keyStorer *KeyStorer) makeKeyFilePath(keyFileName string, keyDirPath strin
 	return filepath.Join(keyDirPath, keyFileName), nil
 }
 
-// makeJsonKeyFile marshals keyFile struct to json format.
-func (keyStorer *KeyStorer) makeJsonKeyFile(kdfSalt []byte, ski []byte, keyGenOpt heimdall.KeyGenOpts, encryptedKeyBytes []byte, keyType heimdall.KeyType) ([]byte, error) {
-	encHints := EncryptionHints{
-		EncInnerFileInfo: keyStorer.encOpt.ToInnerFileInfo(),
-		KDFInnerFileInfo: keyStorer.kdfOpt.ToInnerFileInfo(),
-		KDFSalt:          kdfSalt,
+func makeEncryptionHints(encOpt *encryption.Opts, kdfOpt *kdf.Opts, kdfSalt []byte) *EncryptionHints {
+	return &EncryptionHints{
+		EncOpt:  encOpt,
+		KDFOpt:  kdfOpt,
+		KDFSalt: kdfSalt,
 	}
+}
 
+// makeJsonKeyFile marshals keyFile struct to json format.
+func makeJsonKeyFile(encHints *EncryptionHints, ski []byte, keyGenOpt heimdall.KeyGenOpts, encryptedKeyBytes []byte, isPrivate bool) ([]byte, error) {
 	keyFile := KeyFile{
 		SKI:          ski,
 		KeyGenOpt:    keyGenOpt.ToString(),
-		KeyType:      int(keyType),
+		IsPrivate:    isPrivate,
 		EncryptedKey: hex.EncodeToString(encryptedKeyBytes),
 		Hints:        encHints,
 	}
@@ -147,23 +135,8 @@ func (keyStorer *KeyStorer) makeJsonKeyFile(kdfSalt []byte, ski []byte, keyGenOp
 	return json.Marshal(keyFile)
 }
 
-// KeyLoader is an implementation of heimdall.keyLoader
-type KeyLoader struct {
-	heimdall.KeyDecryptor
-	heimdall.KeyRecoverer
-	heimdall.KeyDeriver
-}
-
-func NewKeyLoader(keyDecryptor heimdall.KeyDecryptor, keyRecoverer heimdall.KeyRecoverer, keyDeriver heimdall.KeyDeriver) heimdall.KeyLoader {
-	return &KeyLoader{
-		KeyDecryptor: keyDecryptor,
-		KeyRecoverer: keyRecoverer,
-		KeyDeriver:   keyDeriver,
-	}
-}
-
 // LoadKey loads private key by key ID and password.
-func (keyLoader *KeyLoader) LoadKey(keyId heimdall.KeyID, pwd string, keyDirPath string) (heimdall.Key, error) {
+func LoadKey(keyId heimdall.KeyID, pwd string, keyDirPath string, recoverer heimdall.KeyRecoverer) (heimdall.Key, error) {
 	var keyFile KeyFile
 
 	if _, err := os.Stat(keyDirPath); os.IsNotExist(err) {
@@ -174,12 +147,12 @@ func (keyLoader *KeyLoader) LoadKey(keyId heimdall.KeyID, pwd string, keyDirPath
 		return nil, err
 	}
 
-	keyPath, err := keyLoader.findKeyById(keyId, keyDirPath)
+	keyPath, err := findKeyById(keyId, keyDirPath)
 	if err != nil {
 		return nil, err
 	}
 
-	jsonKeyFile, err := keyLoader.loadJsonKeyFile(keyPath)
+	jsonKeyFile, err := loadJsonKeyFile(keyPath)
 	if err != nil {
 		return nil, err
 	}
@@ -192,12 +165,17 @@ func (keyLoader *KeyLoader) LoadKey(keyId heimdall.KeyID, pwd string, keyDirPath
 		return nil, err
 	}
 
-	kdfOpt := kdf.MapToOpts(keyFile.Hints.KDFInnerFileInfo)
-	if !kdfOpt.IsValid() {
-		return nil, ErrInvalidKDFOpt
+	kdfOpt, err := kdf.NewOpts(keyFile.Hints.KDFOpt.KdfName, keyFile.Hints.KDFOpt.KdfParams)
+	if err != nil {
+		return nil, err
 	}
 
-	dKey, err := keyLoader.DeriveKey([]byte(pwd), keyFile.Hints.KDFSalt, keyFile.Hints.EncInnerFileInfo.KeyLen, kdfOpt)
+	encOpt, err := encryption.NewOpts(keyFile.Hints.EncOpt.Algorithm, keyFile.Hints.EncOpt.KeyLen, keyFile.Hints.EncOpt.OpMode)
+	if err != nil {
+		return nil, err
+	}
+
+	dKey, err := kdf.DeriveKey([]byte(pwd), keyFile.Hints.KDFSalt, encOpt.KeyLen, kdfOpt)
 	if err != nil {
 		return nil, err
 	}
@@ -207,12 +185,12 @@ func (keyLoader *KeyLoader) LoadKey(keyId heimdall.KeyID, pwd string, keyDirPath
 		return nil, err
 	}
 
-	keyBytes, err := keyLoader.DecryptKey(encryptedKeyBytes, dKey)
+	keyBytes, err := encryption.DecryptKey(encryptedKeyBytes, dKey, encOpt)
 	if err != nil {
 		return nil, err
 	}
 
-	key, err := keyLoader.RecoverKeyFromByte(keyBytes, heimdall.KeyType(keyFile.KeyType), keyFile.KeyGenOpt)
+	key, err := recoverer.RecoverKeyFromByte(keyBytes, keyFile.IsPrivate)
 	if err != nil {
 		return nil, err
 	}
@@ -221,7 +199,7 @@ func (keyLoader *KeyLoader) LoadKey(keyId heimdall.KeyID, pwd string, keyDirPath
 }
 
 // findKeyById finds key file path by key id from file names in keystore path.
-func (keyLoader *KeyLoader) findKeyById(keyId string, keyDirPath string) (keyPath string, err error) {
+func findKeyById(keyId string, keyDirPath string) (keyPath string, err error) {
 	keyPath = ""
 
 	files, err := ioutil.ReadDir(keyDirPath)
@@ -244,7 +222,7 @@ func (keyLoader *KeyLoader) findKeyById(keyId string, keyDirPath string) (keyPat
 }
 
 // loadJsonKeyFile reads json formatted KeyFile struct from file.
-func (keyLoader *KeyLoader) loadJsonKeyFile(keyPath string) (jsonKeyFile []byte, err error) {
+func loadJsonKeyFile(keyPath string) (jsonKeyFile []byte, err error) {
 	if len(keyPath) == 0 {
 		return nil, ErrEmptyKeyPath
 	}
